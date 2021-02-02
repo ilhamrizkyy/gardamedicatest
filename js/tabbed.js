d3666701b2e19a4637266634479a266477882276
ilham rizky <ilhamrizkyakbar@gmail.com>
2021-02-02 14:31:30 +0700

    initial commit

--- js/tabbed.js
+++ js/tabbed.js
@@ -1,10 +1,14 @@
-function toppingCake(toppingName) {
-  let i;
-  const content = document.getElementsByClassName("content");
 
-  for (i = 0; i < content.length; i++) {
-    content[i].style.display = "none";
+function openTab(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
-
-  document.getElementById(toppingName).style.display = "block";
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
